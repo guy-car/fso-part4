069d188dd372f11261ec776a8d0901bfc27a5751
guy-car <guil.crn@gmail.com>
2025-04-24 13:29:43 -0400

    fixed lint bugs, now stable

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -2,8 +2,8 @@ const app = require('./app')
 const config = require('./utils/config')
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
-    console.log('successful GET for 3003')
+  response.send('<h1>Hello World</h1>')
+  console.log('successful GET for 3003')
 }
 )
 
